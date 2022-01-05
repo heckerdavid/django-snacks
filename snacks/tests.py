ea61445d3dec9cdc81de52df8d961d2ac4786729
David Hecker <heckerdw@gmail.com>
2022-01-05 13:50:37 -0800

    Added home page and tests.

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+class SnacksTestCase(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
-# Create your tests here.
+    def test_home_page_html(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
